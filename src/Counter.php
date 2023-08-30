fb10445e3a433fbd23061e94784dbe814e82b466
Saeful Ardi <saeful.4rdi@gmail.com>
2023-08-31 04:23:28 +0700

    Belajar php unit test

---
+++ src/Counter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace webdev\Test;
+
+class Counter {
+    private int $counter = 0;
+
+    public function increment()
+    {
+        $this->counter++;
+    }
+
+    public function getCounter()
+    {
+        return $this->counter;
+    }
+}
---
+++ tests/CounterTest.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace webdev\Test;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class CounterTest extends TestCase
+{
+    public function testCounter() {
+        $counter = new Counter();
+
+        $counter->increment();
+        Assert::assertEquals(1, $counter->getCounter());
+
+        $counter->increment();
+        $this::assertEquals(2, $counter->getCounter());
+
+        $counter->increment();
+        self::assertEquals(3, $counter->getCounter());
+
+    }
+
+    /** 
+     * @test
+    */
+    
+    public function increament()
+    {
+        $counter = new Counter();
+
+        $counter->increment();
+        Assert::assertEquals(1, $counter->getCounter());
+    }
+
+}
